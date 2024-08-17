b3808abc5ec11ab94ddcd76585a28fe83b4d9295
Vasco Costa <vascomacosta@gmail.com>
2024-08-17 22:18:27 +0100

    Bump version

--- src/ui.rs
+++ src/ui.rs
@@ -4,7 +4,7 @@ use bevy_ecs_ldtk::prelude::*;
 use crate::player::PlayerStats;
 
 const APP_NAME: &str = "GLULANDS";
-const VERSION: &str = "v0.3.0";
+const VERSION: &str = "v0.3.1";
 const BAR_COLOR: Color = Color::srgb(0.25, 0.25, 0.25);
 const TEXT_COLOR: Color = Color::srgb(0.1, 1.0, 0.7);
 const GAME_OVER_COLOR: Color = Color::srgb(0.7, 0.2, 0.3);
