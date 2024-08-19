5e76bbd824bdd4c2f51fdbbeab5adeff749e29b2
Vasco Costa <vascomacosta@gmail.com>
2024-08-19 17:25:38 +0100

    Rename out to glulands

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --no-typescript --target web --out-dir ./out/ --out-name "glulands" ./target/wasm32-unknown-unknown/release/glulands.wasm
-tar zcvf glulands_game.tgz out
+tar zcvf glulands.tgz --transform 's/^out/glulands/' out
